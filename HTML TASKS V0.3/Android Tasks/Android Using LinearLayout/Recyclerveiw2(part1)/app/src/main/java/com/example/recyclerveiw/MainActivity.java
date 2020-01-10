f652ef1d47bb488b30643407aa95db7a58ad8154
priyanka <kulkarnip062@gmail.com>
2020-01-10 12:54:57 +0530

    Assignment_Android_RecyclerView_Adapter V0.1

---
+++ HTML TASKS V0.3/Android Tasks/Android Using LinearLayout/Recyclerveiw2(part1)/app/src/main/java/com/example/recyclerveiw/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.recyclerveiw;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+   Adapter adapter;
+   ArrayList<String> items;
+
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        items=new ArrayList<>();
+        items.add("");
+
+
+
+        recyclerView=findViewById(R.id.item);
+        recyclerView.setLayoutManager(new LinearLayoutManager( this));
+        adapter=new Adapter(this,items);
+        recyclerView.setAdapter(adapter);
+    }
+}
