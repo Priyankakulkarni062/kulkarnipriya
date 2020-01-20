8ee9b384687b672a85d6e9583af2ba87d4da3a3f
priyanka <kulkarnip062@gmail.com>
2020-01-20 15:46:48 +0530

    Assignment_Android_RequisitionForm5 V0.2

---
+++ HTML TASKS V0.3/Android Tasks/Android Using RelativeLayout/Recyclerveiw2/app/src/main/java/com/example/recyclerveiw/Adapter.java
@@ -0,0 +1,55 @@
+package com.example.recyclerveiw;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    private LayoutInflater LayoutInflater;
+    private List<String> data;
+
+ Adapter(Context context,List<String> data) {
+this.LayoutInflater= LayoutInflater.from(context);
+this.data=data;
+ }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+
+
+        View view=LayoutInflater.inflate(R.layout.cardview,viewGroup,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+
+     String item1=data.get(i);
+        viewHolder.textitem1.setText(item1);
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+     TextView textitem1,textitem2;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            textitem1=itemView.findViewById(R.id.item1);
+            textitem2=itemView.findViewById(R.id.item2);
+        }
+    }
+
+}
