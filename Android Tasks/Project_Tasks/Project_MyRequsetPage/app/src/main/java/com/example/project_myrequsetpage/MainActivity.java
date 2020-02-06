ddabf7fa67289886c1ff5ed8770b7c7b7e261f4b
priyanka <kulkarnip062@gmail.com>
2020-02-06 12:40:41 +0530

    Assignment_Android_MyRequestPage V0.2

---
+++ Android Tasks/Project_Tasks/Project_MyRequsetPage/app/src/main/java/com/example/project_myrequsetpage/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.project_myrequsetpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    Adapter adapter;
+    ArrayList<String> items;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        items=new ArrayList<>();
+
+        items.add("AWAITING APPROVAL ");
+        items.add("APPROVED ");
+        items.add("DRAFT");
+        items.add("REJECTED");
+
+
+        recyclerView=findViewById(R.id.item);
+        recyclerView.setLayoutManager(new LinearLayoutManager( this));
+        adapter=new Adapter(this,items);
+        recyclerView.setAdapter(adapter);
+    }
+}
