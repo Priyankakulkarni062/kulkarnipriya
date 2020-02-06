74198e03ec31abb6682cb1502e49cb1fe753ae92
priyanka <kulkarnip062@gmail.com>
2020-02-06 13:44:29 +0530

    Assignment_Android_Requisition3Page2 V0.1

---
+++ Android Tasks/Project_Tasks/Project_RequisitionForm3AttachFiles12Page/app/src/main/java/com/example/project_requisitionform3attachfiles12page/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.project_requisitionform3attachfiles12page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
